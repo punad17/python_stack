ff45f1d65ce0fc4312ee83496ffc9df610f5f456
punad17 <nadputrinadia009@gmail.com>
2025-09-29 15:28:58 +0700

    first commit

--- Stack1_240705129.py
+++ Stack1_240705129.py
@@ -2,24 +2,28 @@ import os
 from queue import LifoQueue
 from collections import deque
 
+# Fungsi clear screen
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 print()
-maxSize  = int(input('Masukkan Jumlah data yang ingin ditambah:'))
+maxSize  = int(input('Masukkan Jumlah data yang ingin ditambah: '))
 
 DequeStack = deque()
 LifoStack = LifoQueue(maxsize=maxSize)
 
 cek = True
 while cek:
-    os.system('cis')
+    clear()
     print('Masukkan Pilihan anda-')
     print(' 1. Stack dengan Deque')
     print(' 2. Stack dengan Lifoqueue')
     print(' 0. Keluar')
 
-    pil =int(input('Masukkan Pilihan anda'))
+    pilMenu = int(input('Masukkan Pilihan anda: '))
 
-    if pil ==1:
-        os.system('csl')
+    if pilMenu == 1:
+        clear()
         i = 1
         temp = True
         while temp:
@@ -31,17 +35,73 @@ while cek:
             print('4. Jumlah Data Deque')
             print('0. Keluar')
 
-            if pilMenu ==1:
-                if len(DequeStack) <= maxSize:
+            pilihan = int(input('Masukkan pilihan anda: '))
+
+            if pilihan == 1:
+                if len(DequeStack) < maxSize:
                     while i <= maxSize:
-                        item = input (f'Masukkan data ke {i}:')
+                        item = input(f'Masukkan data ke-{i}: ')
                         DequeStack.append(item)
-                        i+=1
+                        i += 1
                 else:
                     print('Data tidak bisa ditambah. Stack sudah penuh!!')
 
-            elif pilMenu == 2:
-                if len(DequeStack) !=0:
-                    print(f'Elemen terakhir:{DequeStack.pop()} data telah dihapus')
+            elif pilihan == 2:
+                if len(DequeStack) != 0:
+                    print(f'Elemen terakhir: {DequeStack.pop()} telah dihapus')
+                else:
+                    print('Stack kosong. Tidak ada elemen untuk dihapus!!')
+
+            elif pilihan == 3:
+                print('Data dalam stack adalah:')
+                print(DequeStack)
+
+            elif pilihan == 4:
+                print(f'Jumlah Data dalam Stack = {len(DequeStack)}')
+
             else:
-                print('Stack kosong. Tidak ada elemen untuk dihapus!!')
+                temp = False
+                break 
+
+    elif pilMenu == 2:
+        clear()
+        temp = True
+        while temp:
+            print()
+            print('Masukkan Pilihan Anda')
+            print(' 1. Tambah Data dengan LifoQueue')
+            print(' 2. Hapus Data LifoQueue')
+            print(' 3. Tampil Data LifoQueue')
+            print(' 4. Jumlah Data LifoQueue')
+            print(' 0. Keluar')
+
+            pilihan = int(input('Masukkan pilihan anda: '))
+
+            if pilihan == 1:
+                if LifoStack.full() == False:
+                    i = LifoStack.qsize() + 1
+                    while i <= maxSize:
+                        item = input(f'Masukkan data ke-{i}: ')
+                        LifoStack.put(item)
+                        i += 1
+                else:
+                    print('Data tidak bisa ditambah. Stack sudah penuh!.')
+            elif pilihan == 2:
+                if not LifoStack.empty():
+                    print(f'Elemen terakhir: {LifoStack.get()} telah dihapus')
+                else:
+                    print('Stack kosong. Tidak ada elemen untuk dihapus!!')
+            elif pilihan == 3:
+                isi = list(LifoStack.queue)
+                print('Data dalam Stack adalah:')
+                print(isi)
+            elif pilihan == 4:
+                print(f'Jumlah Data dalam Stack = {LifoStack.qsize()}')
+            else:
+                temp = False
+                break
+    elif pilMenu == 0:
+        cek = False
+        break
+    else:
+        print('Pilihan tidak ada')
